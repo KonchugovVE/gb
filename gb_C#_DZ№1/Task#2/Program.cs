b33cccba03493494b09ca3c8fc1fac6a725323d3
Vladislav <konchugovve@gmail.com>
2023-06-13 16:45:32 +0300

    array creation

---
+++ gb_C#_DZ№1/Task#2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i = 0;
+Console.Write("Введите колличество эемепнтов массива: ");
+
+int elCount = int.Parse(Console.ReadLine());
+
+int[] myArray = new int[elCount];
+
+for (i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите значение массива под индексом {i}: ");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("ввывод массива:");
+
+for (i = 0; i < myArray.Length; i++)
+{ Console.Write(myArray[i]); }
